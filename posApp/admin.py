00efc9ef16191ecff710b3f6a8817651f9da6d61
Tahmid-352 <cssmith804@gmail.com>
2023-09-14 17:39:58 +0600

    first commit

---
+++ posApp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from posApp.models import Category, Products, Sales, salesItems, CategoryOfExpense, ProductsOfExpense, Employees
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Products)
+admin.site.register(Sales)
+admin.site.register(salesItems)
+admin.site.register(CategoryOfExpense)
+admin.site.register(ProductsOfExpense)
+
+admin.site.register(Employees)
---
+++ posApp/views.py
@@ -0,0 +1,528 @@
+from pickle import FALSE
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from flask import jsonify
+from posApp.models import Category, Products, Sales, salesItems, CategoryOfExpense, ProductsOfExpense, Employees
+from django.db.models import Count, Sum
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+import json, sys
+from datetime import date, datetime
+
+# Login
+def login_user(request):
+    logout(request)
+    resp = {"status":'failed','msg':''}
+    username = ''
+    password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                resp['status']='success'
+            else:
+                resp['msg'] = "Incorrect username or password"
+        else:
+            resp['msg'] = "Incorrect username or password"
+    return HttpResponse(json.dumps(resp),content_type='application/json')
+
+#Logout
+def logoutuser(request):
+    logout(request)
+    return redirect('/')
+
+# Create your views here.
+@login_required
+def home(request):
+    now = datetime.now()
+    current_year = now.strftime("%Y")
+    current_month = now.strftime("%m")
+    current_day = now.strftime("%d")
+    categories = len(Category.objects.all())
+    products = len(Products.objects.all())
+    transaction = len(Sales.objects.filter(
+        date_added__year=current_year,
+        date_added__month = current_month,
+        date_added__day = current_day
+    ))
+    today_sales = Sales.objects.filter(
+        date_added__year=current_year,
+        date_added__month = current_month,
+        date_added__day = current_day
+    ).all()
+    total_sales = sum(today_sales.values_list('grand_total',flat=True))
+
+    this_months_transaction = len(Sales.objects.filter(
+        date_added__year=current_year,
+        date_added__month = current_month,
+    ))
+    
+    this_months_sale = Sales.objects.filter(
+        date_added__year=current_year,
+        
+    ).all()
+    total_sales_on_month = sum(this_months_sale.values_list('grand_total', flat=True))
+
+    this_months_expense = ProductsOfExpense.objects.filter(
+        date_added__year=current_year,
+        date_added__month = current_month,
+    ).all()
+    total_expense_on_month = sum(this_months_expense.values_list('price', flat=True))
+
+    total_profit_on_month = total_sales_on_month - total_expense_on_month
+    
+    context = {
+        'page_title':'Home',
+        'categories' : categories,
+        'products' : products,
+        'transaction' : transaction,
+        'total_sales' : total_sales,
+        'total_sales_on_month': total_sales_on_month,
+        'this_months_transaction': this_months_transaction,
+        'total_expense': total_expense_on_month,
+        'profit_on_month': total_profit_on_month,
+    }
+    return render(request, 'posApp/home.html',context)
+
+
+def about(request):
+    context = {
+        'page_title':'About',
+    }
+    return render(request, 'posApp/about.html',context)
+
+#Categories
+@login_required
+def category(request):
+    category_list = Category.objects.all()
+    # category_list = {}
+    context = {
+        'page_title':'Category List',
+        'category':category_list,
+    }
+    return render(request, 'posApp/category.html',context)
+
+@login_required
+def manage_category(request):
+    category = {}
+    if request.method == 'GET':
+        data =  request.GET
+        id = ''
+        if 'id' in data:
+            id= data['id']
+        if id.isnumeric() and int(id) > 0:
+            category = Category.objects.filter(id=id).first()
+    
+    context = {
+        'category' : category
+    }
+    return render(request, 'posApp/manage_category.html',context)
+
+@login_required
+def save_category(request):
+    data =  request.POST
+    resp = {'status':'failed'}
+    try:
+        if (data['id']).isnumeric() and int(data['id']) > 0 :
+            save_category = Category.objects.filter(id = data['id']).update(name=data['name'], description = data['description'],status = data['status'])
+        else:
+            save_category = Category(name=data['name'], description = data['description'],status = data['status'])
+            save_category.save()
+        resp['status'] = 'success'
+        messages.success(request, 'Category Successfully saved.')
+    except:
+        resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+@login_required
+def delete_category(request):
+    data =  request.POST
+    resp = {'status':''}
+    try:
+        Category.objects.filter(id = data['id']).delete()
+        resp['status'] = 'success'
+        messages.success(request, 'Category Successfully deleted.')
+    except:
+        resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+# Products
+@login_required
+def products(request):
+    product_list = Products.objects.all()
+    context = {
+        'page_title':'Product List',
+        'products':product_list,
+    }
+    return render(request, 'posApp/products.html',context)
+@login_required
+def manage_products(request):
+    product = {}
+    categories = Category.objects.filter(status = 1).all()
+    if request.method == 'GET':
+        data =  request.GET
+        id = ''
+        if 'id' in data:
+            id= data['id']
+        if id.isnumeric() and int(id) > 0:
+            product = Products.objects.filter(id=id).first()
+    
+    context = {
+        'product' : product,
+        'categories' : categories
+    }
+    return render(request, 'posApp/manage_product.html',context)
+def test(request):
+    categories = Category.objects.all()
+    context = {
+        'categories' : categories
+    }
+    return render(request, 'posApp/test.html',context)
+@login_required
+def save_product(request):
+    data =  request.POST
+    resp = {'status':'failed'}
+    id= ''
+    if 'id' in data:
+        id = data['id']
+    if id.isnumeric() and int(id) > 0:
+        check = Products.objects.exclude(id=id).filter(code=data['code']).all()
+    else:
+        check = Products.objects.filter(code=data['code']).all()
+    if len(check) > 0 :
+        resp['msg'] = "Product Code Already Exists in the database"
+    else:
+        category = Category.objects.filter(id = data['category_id']).first()
+        try:
+            if (data['id']).isnumeric() and int(data['id']) > 0 :
+                save_product = Products.objects.filter(id = data['id']).update(code=data['code'], category_id=category, name=data['name'], description = data['description'], price = float(data['price']),status = data['status'])
+            else:
+                save_product = Products(code=data['code'], category_id=category, name=data['name'], description = data['description'], price = float(data['price']),status = data['status'])
+                save_product.save()
+            resp['status'] = 'success'
+            messages.success(request, 'Product Successfully saved.')
+        except:
+            resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+@login_required
+def delete_product(request):
+    data =  request.POST
+    resp = {'status':''}
+    try:
+        Products.objects.filter(id = data['id']).delete()
+        resp['status'] = 'success'
+        messages.success(request, 'Product Successfully deleted.')
+    except:
+        resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+@login_required
+def pos(request):
+    products = Products.objects.filter(status = 1)
+    product_json = []
+    for product in products:
+        product_json.append({'id':product.id, 'name':product.name, 'price':float(product.price)})
+    context = {
+        'page_title' : "Point of Sale",
+        'products' : products,
+        'product_json' : json.dumps(product_json)
+    }
+    # return HttpResponse('')
+    return render(request, 'posApp/pos.html',context)
+
+@login_required
+def checkout_modal(request):
+    grand_total = 0
+    if 'grand_total' in request.GET:
+        grand_total = request.GET['grand_total']
+    context = {
+        'grand_total' : grand_total,
+    }
+    return render(request, 'posApp/checkout.html',context)
+
+@login_required
+def save_pos(request):
+    resp = {'status':'failed','msg':''}
+    data = request.POST
+    pref = datetime.now().year + datetime.now().year
+    i = 1
+    while True:
+        code = '{:0>5}'.format(i)
+        i += int(1)
+        check = Sales.objects.filter(code = str(pref) + str(code)).all()
+        if len(check) <= 0:
+            break
+    code = str(pref) + str(code)
+
+    try:
+        sales = Sales(code=code, sub_total = data['sub_total'], tax = data['tax'], tax_amount = data['tax_amount'], grand_total = data['grand_total'], tendered_amount = data['tendered_amount'], amount_change = data['amount_change']).save()
+        sale_id = Sales.objects.last().pk
+        i = 0
+        for prod in data.getlist('product_id[]'):
+            product_id = prod 
+            sale = Sales.objects.filter(id=sale_id).first()
+            product = Products.objects.filter(id=product_id).first()
+            qty = data.getlist('qty[]')[i] 
+            price = data.getlist('price[]')[i] 
+            total = float(qty) * float(price)
+            print({'sale_id' : sale, 'product_id' : product, 'qty' : qty, 'price' : price, 'total' : total})
+            salesItems(sale_id = sale, product_id = product, qty = qty, price = price, total = total).save()
+            i += int(1)
+        resp['status'] = 'success'
+        resp['sale_id'] = sale_id
+        messages.success(request, "Sale Record has been saved.")
+    except:
+        resp['msg'] = "An error occured"
+        print("Unexpected error:", sys.exc_info()[0])
+    return HttpResponse(json.dumps(resp),content_type="application/json")
+
+@login_required
+def salesList(request):
+    sales = Sales.objects.all()
+    sale_data = []
+    for sale in sales:
+        data = {}
+        for field in sale._meta.get_fields(include_parents=False):
+            if field.related_model is None:
+                data[field.name] = getattr(sale,field.name)
+        data['items'] = salesItems.objects.filter(sale_id = sale).all()
+        data['item_count'] = len(data['items'])
+        if 'tax_amount' in data:
+            data['tax_amount'] = format(float(data['tax_amount']),'.2f')
+        # print(data)
+        sale_data.append(data)
+    # print(sale_data)
+    context = {
+        'page_title':'Sales Transactions',
+        'sale_data':sale_data,
+    }
+    # return HttpResponse('')
+    return render(request, 'posApp/sales.html',context)
+
+@login_required
+def receipt(request):
+    id = request.GET.get('id')
+    sales = Sales.objects.filter(id = id).first()
+    transaction = {}
+    for field in Sales._meta.get_fields():
+        if field.related_model is None:
+            transaction[field.name] = getattr(sales,field.name)
+    if 'tax_amount' in transaction:
+        transaction['tax_amount'] = format(float(transaction['tax_amount']))
+    ItemList = salesItems.objects.filter(sale_id = sales).all()
+    context = {
+        "transaction" : transaction,
+        "salesItems" : ItemList
+    }
+
+    return render(request, 'posApp/receipt.html',context)
+    # return HttpResponse('')
+
+@login_required
+def delete_sale(request):
+    resp = {'status':'failed', 'msg':''}
+    id = request.POST.get('id')
+    try:
+        delete = Sales.objects.filter(id = id).delete()
+        resp['status'] = 'success'
+        messages.success(request, 'Sale Record has been deleted.')
+    except:
+        resp['msg'] = "An error occured"
+        print("Unexpected error:", sys.exc_info()[0])
+    return HttpResponse(json.dumps(resp), content_type='application/json')
+
+
+
+
+# Expense Category
+
+@login_required
+def expenseCategory(request):
+    category_list = CategoryOfExpense.objects.all()
+    # category_list = {}
+    context = {
+        'page_title':'Expense Category List',
+        'expenseCategory':category_list,
+    }
+    return render(request, 'posApp/expenseCategory.html',context)
+
+@login_required
+def manage_expenseCategory(request):
+    category = {}
+    if request.method == 'GET':
+        data =  request.GET
+        id = ''
+        if 'id' in data:
+            id= data['id']
+        if id.isnumeric() and int(id) > 0:
+            category = CategoryOfExpense.objects.filter(id=id).first()
+    
+    context = {
+        'expenseCategory' : category
+    }
+    return render(request, 'posApp/manage_expenseCategory.html',context)
+
+@login_required
+def saveExpenseCategory(request):
+    data =  request.POST
+    resp = {'status':'failed'}
+    try:
+        if (data['id']).isnumeric() and int(data['id']) > 0 :
+            save_category = CategoryOfExpense.objects.filter(id = data['id']).update(name=data['name'], description = data['description'],status = data['status'])
+        else:
+            save_category = CategoryOfExpense(name=data['name'], description = data['description'],status = data['status'])
+            save_category.save()
+        resp['status'] = 'success'
+        messages.success(request, 'Category Successfully saved.')
+    except:
+        resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+@login_required
+def deleteExpenseCategory(request):
+    data =  request.POST
+    resp = {'status':''}
+    try:
+        CategoryOfExpense.objects.filter(id = data['id']).delete()
+        resp['status'] = 'success'
+        messages.success(request, 'Category Successfully deleted.')
+    except:
+        resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+# Expense products list
+
+@login_required
+def expenseProducts(request):
+    product_list = ProductsOfExpense.objects.all()
+    context = {
+        'page_title':'Product List',
+        'expenseProducts':product_list,
+    }
+    return render(request, 'posApp/expenseProducts.html',context)
+
+
+@login_required
+def manage_expenseProducts(request):
+    product = {}
+    categories = CategoryOfExpense.objects.filter(status = 1).all()
+    if request.method == 'GET':
+        data =  request.GET
+        id = ''
+        if 'id' in data:
+            id= data['id']
+        if id.isnumeric() and int(id) > 0:
+            product = ProductsOfExpense.objects.filter(id=id).first()
+    
+    context = {
+        'expenseProduct' : product,
+        'expenseCategories' : categories
+    }
+    return render(request, 'posApp/manage_expenseProducts.html',context)
+
+
+@login_required
+def save_expenseProduct(request):
+    data =  request.POST
+    resp = {'status':'failed'}
+    id= ''
+    if 'id' in data:
+        id = data['id']
+    if id.isnumeric() and int(id) > 0:
+        check = ProductsOfExpense.objects.exclude(id=id).filter(code=data['code']).all()
+    else:
+        check = ProductsOfExpense.objects.filter(code=data['code']).all()
+    if len(check) > 0 :
+        resp['msg'] = "Product Code Already Exists in the database"
+    else:
+        category = CategoryOfExpense.objects.filter(id = data['category_id']).first()
+        try:
+            if (data['id']).isnumeric() and int(data['id']) > 0 :
+                save_product = ProductsOfExpense.objects.filter(id = data['id']).update(code=data['code'], category_id=category, name=data['name'], description = data['description'], price = float(data['price']),status = data['status'])
+            else:
+                save_product = ProductsOfExpense(code=data['code'], category_id=category, name=data['name'], description = data['description'], price = float(data['price']),status = data['status'])
+                save_product.save()
+            resp['status'] = 'success'
+            messages.success(request, 'Product Successfully saved.')
+        except:
+            resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+@login_required
+def delete_expenseProduct(request):
+    data =  request.POST
+    resp = {'status':''}
+    try:
+        ProductsOfExpense.objects.filter(id = data['id']).delete()
+        resp['status'] = 'success'
+        messages.success(request, 'Product Successfully deleted.')
+    except:
+        resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+
+@login_required
+def employees(request):
+    employees_list = Employees.objects.all()
+    # category_list = {}
+    context = {
+        'page_title':'Employee List',
+        'employees': employees_list,
+    }
+    return render(request, 'posApp/employees.html',context)
+
+
+@login_required
+def manage_employees(request):
+    employee = {}
+    if request.method == 'GET':
+        data =  request.GET
+        id = ''
+        if 'id' in data:
+            id= data['id']
+        if id.isnumeric() and int(id) > 0:
+            employee = Employees.objects.filter(id=id).first()
+    
+    context = {
+        'employee' : employee,
+    }
+    return render(request, 'posApp/manage_employees.html',context)
+
+
+@login_required
+def save_employee(request):
+    data =  request.POST
+    resp = {'status':'failed'}
+    try:
+        if (data['id']).isnumeric() and int(data['id']) > 0 :
+            save_employee = Employees.objects.filter(id = data['id']).update(name=data['name'], description = data['description'], image= data['image'], phone= data['phone'], salary= data['salary'], hiredDate= data['hiredDate'], status = data['status'])
+        else:
+            save_employee = Employees(name=data['name'], description = data['description'], image= data['image'], phone= data['phone'], salary= data['salary'], hiredDate= data['hiredDate'], status = data['status'])
+            save_employee.save()
+        resp['status'] = 'success'
+        messages.success(request, 'Employee Successfully saved.')
+    except:
+        resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+
+@login_required
+def delete_employee(request):
+    data =  request.POST
+    resp = {'status':''}
+    try:
+        Employees.objects.filter(id = data['id']).delete()
+        resp['status'] = 'success'
+        messages.success(request, 'Employee Successfully deleted.')
+    except:
+        resp['status'] = 'failed'
+    return HttpResponse(json.dumps(resp), content_type="application/json")
